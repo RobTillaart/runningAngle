efe014dc5fbbc76e3f2a7b67eceacd56c2ae46db
Rob Tillaart <rob.tillaart@gmail.com>
2021-01-14 10:00:39 +0100

    update unit test + readme (#7)
    * update unit test + readme

--- test/unit_test_001.cpp
+++ test/unit_test_001.cpp
@@ -50,29 +50,65 @@ unittest(test_constructor)
   runningAngle heading(runningAngle::DEGREES);
   assertEqualFloat(0.80, heading.getWeight(), 0.0001);  
   assertEqualFloat(0, heading.getAverage(), 0.0001);
+}
 
 
-  fprintf(stderr, "\n");
+unittest(test_getAverage)
+{
+  runningAngle heading(runningAngle::DEGREES);
+
   for (int angle = 0; angle < 10; angle++)
   {
     heading.add(angle);
     fprintf(stderr, "%d\t%f\n", angle, heading.getAverage());
   }
   assertEqualFloat(8.750000, heading.getAverage(), 0.0001);
+  fprintf(stderr, "\treset()\n");
+  heading.reset();
+  assertEqualFloat(0, heading.getAverage(), 0.0001);
+
+}
 
 
-  fprintf(stderr, "\n");
+unittest(test_weight)
+{
+  runningAngle heading(runningAngle::DEGREES);
+  assertEqualFloat(0.80, heading.getWeight(), 0.0001);  
+
   heading.setWeight(0.85);
   assertEqualFloat(0.85, heading.getWeight(), 0.0001);
+
   heading.setWeight(2);
   assertEqualFloat(1, heading.getWeight(), 0.0001);
+
   heading.setWeight(-5);
   assertEqualFloat(0.001, heading.getWeight(), 0.0001);
 
-  fprintf(stderr, "\n");
-  assertEqualFloat(0, heading.wrap(360), 0.0001);
-
+  fprintf(stderr, "\treset()\n");
+  heading.reset();
+  assertEqualFloat(0.80, heading.getWeight(), 0.0001);
 }
+
+
+unittest(test_wrap)
+{
+  runningAngle heading(runningAngle::DEGREES);
+
+  assertEqualFloat(0, heading.wrap(0), 0.0001);
+  assertEqualFloat(0, heading.wrap(360), 0.0001);
+  assertEqualFloat(1, heading.wrap(361), 0.0001);
+  assertEqualFloat(179, heading.wrap(179), 0.0001);
+  assertEqualFloat(-180, heading.wrap(180), 0.0001);
+  assertEqualFloat(-179, heading.wrap(181), 0.0001);
+  assertEqualFloat(-179, heading.wrap(-179), 0.0001);
+  assertEqualFloat(-180, heading.wrap(-180), 0.0001);
+  assertEqualFloat(179, heading.wrap(-181), 0.0001);
+  assertEqualFloat(1, heading.wrap(721), 0.0001);
+  assertEqualFloat(-1, heading.wrap(-1), 0.0001);
+  assertEqualFloat(-1, heading.wrap(-361), 0.0001);
+  assertEqualFloat(-1, heading.wrap(-721), 0.0001);
+}
+
 
 unittest_main()
 
